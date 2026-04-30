7c35986bba64cf234bf438283a4ba5fb7a1f93cd
maria.mendes <maria.mendes@SJ208-06>
2026-04-30 09:24:57 -0300

    Adicionar arquivos de projeto.

---
+++ Pages/QuemSomos.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BeautyClub2026App.Pages
+{
+    public class QuemSomosModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
